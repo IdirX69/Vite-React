fd8f924d114f2b96462f5a5028805dd237484c62
IdirX69 <adil.idirr@gmail.com>
2023-03-28 16:48:10 +0200

    props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const PokemonCard = ({ pokemonList }) => {
   let pokemon = pokemonList[0];
   if (Math.random() > 0.5) {
     pokemon = pokemonList[1];
   }
+  PokemonCard.propTypes = {
+    pokemonList: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      imgSrc: PropTypes.string.isRequired,
+    }).isRequired,
+  };
 
   return (
     <div>
