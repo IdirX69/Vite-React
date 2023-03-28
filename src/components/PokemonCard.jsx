42d29e244c617f6211e84e4c79bfcd980aa9a83f
IdirX69 <adil.idirr@gmail.com>
2023-03-28 15:57:30 +0200

    pokemon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 const PokemonCard = () => {
+  let pokemon = pokemonList[0];
+  if (Math.random() > 0.5) {
+    pokemon = pokemonList[1];
+  }
+
   return (
-    <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt=""
-      />
-      <figcaption>bulbasaur</figcaption>
-    </figure>
+    <div>
+      <figure>
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    </div>
   );
 };
 
