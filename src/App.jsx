070ddafbd23142cd5d9fc03f14fea204a698cc28
IdirX69 <adil.idirr@gmail.com>
2023-03-30 11:12:15 +0200

    state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 import Navbar from "./components/Navbar";
